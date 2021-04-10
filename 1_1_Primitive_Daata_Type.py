fef643c18119f53e1cd59b0b888edecb5900084e
daaep <daaep0724@gmail.com>
2021-04-10 06:24:11 -1000

    Add files via upload

---
+++ 1_1_Primitive_Daata_Type.py
@@ -0,0 +1,15 @@
+#Declare a string variable
+s = 'Planet Earth'
+print(s)
+
+#Declare an integer variable
+x = 5
+print(x)
+
+#Declare a float variable
+y = 12.53
+print (y)
+
+#Declare a boolean variable
+z = x>y
+print (z)
